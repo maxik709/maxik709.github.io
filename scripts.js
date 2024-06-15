9b9fb8af9050251c596ef5c78b129d528494d303
maxik709 <maxik709@gmail.com>
2024-06-15 17:40:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav');
+
+  menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('active');
+  });
+});
